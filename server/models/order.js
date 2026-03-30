1a117ac712d123f3b51fcdabf42f4e3a19c8109f
doflamingo-evil <sangeerthsanthoshik2233@gmail.com>
2026-03-30 22:43:46 +0530

    Add order schema model using mongoose

--- server/models/order.js
+++ server/models/order.js
@@ -1,1 +1,11 @@
+import mongoose from "mongoose";
 
+const orderSchema = new mongoose.Schema({
+  name: String,
+  phone: String,
+  room: String,
+  items: [String],
+  createdAt: { type: Date, default: Date.now }
+});
+
+export default mongoose.model("Order", orderSchema);
