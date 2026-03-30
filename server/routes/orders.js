ada198d1af0ad0ac179891c1f38a629fc9f3fa9c
doflamingo-evil <sangeerthsanthoshik2233@gmail.com>
2026-03-30 22:45:28 +0530

    Add orders route to fetch and sort orders

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -1,1 +1,11 @@
+import express from "express";
+import Order from "../models/order.js";
 
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const orders = await Order.find().sort({ createdAt: -1 });
+  res.json(orders);
+});
+
+export default router;
