4efb22a4d5b87ae99ab19b4d1b1620216cebb461
doflamingo-evil <sangeerthsanthoshik2233@gmail.com>
2026-03-30 22:43:13 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import voiceRoute from "./routes/voice.js";
+import orderRoute from "./routes/orders.js";
 
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// MongoDB connect
+mongoose.connect(process.env.MONGO_URI);
+
+// routes
+app.use("/voice", voiceRoute);
+app.use("/orders", orderRoute);
+
+app.listen(3000, () => console.log("Server running"));
